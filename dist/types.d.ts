23181770503b40822650cc8bd4dd2bb8b92c3326
Alon Novy <alon.novy@gmail.com>
2022-08-30 01:07:13 +1000

    fixed optional props for Widget declaration

--- dist/types.d.ts
+++ dist/types.d.ts
@@ -1,6 +1,6 @@
 /// <reference types="react" />
-export declare type Widget<TProps extends any | void = void> = (props: TProps) => JSX.Element | null;
-export declare type ParentWidget<TProps extends any | void = void> = (props: TProps & {
+export declare type Widget<TProps extends any | void = {}> = (props: TProps) => JSX.Element | null;
+export declare type ParentWidget<TProps extends any | void = {}> = (props: TProps & {
     children?: JSX.Element;
 }) => JSX.Element | null;
 export declare type PageAnimation = "slideFromRight" | "slideToRight" | "slideFromBottom" | "slideToBottom" | "slideFromLeft" | "slideToLeft" | "slideFromTop" | "slideToTop" | "scaleUp" | "scaleDown" | "fadeIn" | "fadeOut" | "spinClockwise" | "spinCounterClockwise" | "flipUp" | "flipDown" | "none";
